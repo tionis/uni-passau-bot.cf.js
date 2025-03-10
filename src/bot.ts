e274299fefeb756d19e96f3284ea6e0ea7c9f182
tionis <me@tionis.dev>
2025-03-10 13:37:22 +0100

    better webui

--- src/bot.ts
+++ src/bot.ts
@@ -2,25 +2,7 @@ import { Env } from './types';
 import { tgSendMessage } from './telegram';
 import { Message } from '@telegraf/types';
 import { getMensaDataForWeek, MensaEntry } from './mensa';
-
-// Returns the ISO week of the date.
-function getISOWeek(date: Date): number {
-	const tempDate = new Date(date.getTime());
-	tempDate.setHours(0, 0, 0, 0);
-	// Thursday in current week decides the year.
-	tempDate.setDate(tempDate.getDate() + 3 - ((tempDate.getDay() + 6) % 7));
-	// January 4 is always in week 1.
-	const week1 = new Date(tempDate.getFullYear(), 0, 4);
-	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
-	return 1 + Math.round(((tempDate.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
-}
-
-// Returns the four-digit year corresponding to the ISO week of the date.
-function getISOWeekYear(date: Date): number {
-	const tempDate = new Date(date.getTime());
-	tempDate.setDate(tempDate.getDate() + 3 - ((tempDate.getDay() + 6) % 7));
-	return tempDate.getFullYear();
-}
+import { getISOWeek, getISOWeekYear } from './util';
 
 async function getMensaInfo(env: Env, weekNumber: number): Promise<MensaEntry[]> {
 	const lookup_key = `mensa/info/${weekNumber}`;
@@ -166,4 +148,4 @@ async function handleMessage(env: Env, message: Message) {
 	// Ignore non text message for now
 }
 
-export { handleMessage };
+export { handleMessage, getPrettyMensaInfoForDate, getPrettyMensaInfoForWeek };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { getMensaDataForWeek } from './mensa';
-import { Hono } from 'hono';
+import { Hono, Context } from 'hono';
+import { html, raw } from 'hono/html';
 import { Env } from './types';
 import { tgGet } from './telegram';
-import { handleMessage } from './bot';
+import { handleMessage, getPrettyMensaInfoForDate, getPrettyMensaInfoForWeek } from './bot';
 import { Update } from '@telegraf/types';
+import { getISOWeek } from './util';
+import { handle } from 'hono/cloudflare-pages';
 
 const webhook_path = '/webhook/update';
 const app = new Hono<{ Bindings: Env }>();
@@ -12,12 +15,81 @@ app.get('/', (c) => {
 	return c.text('Hi!');
 });
 
+async function handle_week(c: Context, week: number) {
+	const format = c.req.query('format');
+	switch (format) {
+		case 'json':
+			return c.json(await getMensaDataForWeek(1));
+		case 'jsonl':
+			c.header('Content-Type', 'application/x-ndjson');
+			const data = await getMensaDataForWeek(week);
+			let jsonlOutput = '';
+			for (const entry of data) {
+				jsonlOutput += JSON.stringify(entry) + '\n';
+			}
+			return c.text(jsonlOutput);
+		case 'pretty':
+			const prettyData = await getPrettyMensaInfoForWeek(c.env, week);
+			return c.text(prettyData);
+		case 'html':
+		case '':
+		case undefined:
+			const mensaData = await getMensaDataForWeek(week);
+			const dates = mensaData
+				.map((entry) => entry.date.toISOString().split('T')[0])
+				.filter((value, index, self) => self.indexOf(value) === index)
+				.sort();
+			return c.html(
+				html`<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<title>mensa</title>
+		<link rel="stylesheet" href="/water.css" />
+	</head>
+	<body>
+		<h1>Speiseplan für KW ${week}</h1>
+${raw(
+			dates
+				.map((date) => {
+					let out = '\t\t\t<h2>' + new Date(date).toLocaleDateString('de-DE', { weekday: 'long' })
+					+', der ' + new Date(date).toLocaleDateString('de-DE') + '</h2>\n';
+					out += '\t\t\t<ul>\n';
+					const entries = mensaData.filter((entry) => entry.date.toISOString().split('T')[0] === date);
+					for (const entry of entries) {
+						out +=
+							'\t\t\t\t<li><strong>' +
+							entry.category +
+							'</strong>: ' +
+							entry.name +
+							' (' +
+							entry.price.stud.toFixed(2).replace('.', ',') +
+							'€)</li>\n';
+					}
+					out += '\t\t\t</ul>\n';
+					return out;
+				})
+				.join('')
+		)}
+	</body>
+</html>`
+			);
+		default:
+			console.log(format);
+			c.status(400);
+			return c.text('Invalid format');
+	}
+}
+
 app.get('/mensa', async (c) => {
-	return c.json(await getMensaDataForWeek(1));
+	const current_week = getISOWeek(new Date());
+	return await handle_week(c, current_week);
 });
 
 app.get('/mensa/:week', async (c) => {
-	return c.json(await getMensaDataForWeek(parseInt(c.req.param('week'))));
+	return await handle_week(c, parseInt(c.req.param('week')));
 });
 
 app.post(webhook_path, async (c) => {
@@ -53,7 +125,11 @@ app.get('/webhook/register', async (c) => {
 			return c.json({ status: 'ok', webhookUrl: webhookUrl });
 		} else {
 			c.status(500);
-			return c.json({ error: 'Failed to register webhook', response: r, webhookUrl: webhookUrl });
+			return c.json({
+				error: 'Failed to register webhook',
+				response: r,
+				webhookUrl: webhookUrl,
+			});
 		}
 	} else {
 		c.status(401);
